6cffeddc57f0b460ae820f4f3fa5bdd72279fdca
luxiaoling <15804068180@163.com>
2016-12-21 22:18:54 +0800

    Signed-off-by: luxiaoling <15804068180@163.com>

---
+++ JavaKeShe/src/ReadTestquestion.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+
+public class ReadTestquestion {
+	//全局变量定义
+String filename="",    //文件名
+correctAnswer="",    //正确答案
+testContent="",  //考试题目内容
+selection="";    //考生答案
+int score=0;     //成绩
+long time=0;     //考试时间
+boolean wcks=false;     //是否完成考试
+File f=null;            //文件
+FileReader in=null;     //从文件中读取内容
+BufferedReader dqxx;    //读取信息
+
+
+
+
+
+}
